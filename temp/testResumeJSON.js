7aea8c9f11cd5cac762b34fa35281755a5b6b049
Andrew Dimmer <contactandrewd@gmail.com>
2019-02-09 04:55:56 -0500

    Added a Test Resume JSON for testing

---
+++ temp/testResumeJSON.js
@@ -0,0 +1,10 @@
+var testResumeJSON = {
+	name: "Test Resume",
+	includeEmail: true,
+	includePhone: true,
+	includeAddress: true,
+	websites: ["web-1","web-2","web-3"],
+	websiteOrder: [1,2,3],
+	skills: ["skill-1", "skill-2", "skill-3"]
+	skillOrder: [1,2,3];
+}
